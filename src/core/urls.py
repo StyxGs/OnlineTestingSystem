f0c2b1c30d287abd2139fd4fc21162d909b56d3d
Georgy <gosha.sychev.98@gmail.com>
2025-06-29 15:23:40 +0300

    add router test and user in core urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.tests.urls', namespace='tests')),
+    path('', include('apps.users.urls', namespace='users')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'api/schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui',
+    ),
 ]
